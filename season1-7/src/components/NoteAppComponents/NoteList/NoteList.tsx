99996304fca0aadb427379bae78660ee38d64b19
Arman Abbasian <abasian.arman@gmail.com>
2023-10-27 12:45:18 +0330

    make the status sectin in noteLIst component

--- season1-7/src/components/NoteAppComponents/NoteList/NoteList.tsx
+++ season1-7/src/components/NoteAppComponents/NoteList/NoteList.tsx
@@ -8,15 +8,31 @@ interface IProps{
   deleteHandler:(id:number)=>void
 
 }
-const status:string[]=["All","completed","open"]
 function NoteList({notes,changeCompleted,deleteHandler}:IProps) {
+  let All=0;
+  let Completed=0;
+  let Open=0
+  if(notes.length>0){
+     All=notes.length;
+    const CompletedNotes=notes.filter(item=>item.completed===true)
+    const OpenNotes=notes.filter(item=>item.completed===false)
+    if(CompletedNotes.length>0){
+      Completed=CompletedNotes.length
+    }
+    if(OpenNotes.length>0){
+      Open=OpenNotes.length
+    }
+  }
+  
   if(notes.length===0) return <div className="NoteListContainer">
     <p style={{textAlign:"center"}}>there is any notes</p>
     </div>
   return (
     <div className="NoteListContainer">
       <div className="NoteListContainer_status">
-        {status.map(item=><p key={item}>{item}</p>)}
+        <p>All {All}</p>
+        <p>Completed {Completed}</p>
+        <p>Open {Open}</p>
       </div>
       <div className="NoteListContainer_NoteList">
        {notes.map((item:INote)=> <Note key={item.id} deleteHandler={()=>deleteHandler(item.id)} changeCompleted={()=>changeCompleted(item.id)} note={item} />)}
