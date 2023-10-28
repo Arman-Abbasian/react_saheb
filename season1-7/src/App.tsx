aeed32abf7a0600c0be3f2c7484e85cf119eb63f
Arman Abbasian <abasian.arman@gmail.com>
2023-10-28 15:00:25 +0330

    some refactor on codes

--- season1-7/src/App.tsx
+++ season1-7/src/App.tsx
@@ -4,32 +4,38 @@ import Header from "./components/NoteAppComponents/Header/Header"
 import Main from "./components/NoteAppComponents/Main/Main"
 import {useState} from 'react';
 
+//! types and interfaces-------------------------
+type TOptions="latest"|"earliest"|"completed"
 //this file is a component and is the father of all the component and this component add to the real dom
 function App() {
+  //! states-------------------------------------
+  const [selectedOption, setSelectedOption]= useState<TOptions>("latest");
   const [notes,setNotes]=useState<INote[]>([])
-  const [selectedOption, setSelectedOption]= useState<"latest"|"earliest"|"completed">("latest");
+  
+  //! all Actions related to "notes" state---------
+  const changeCompleted=(e: React.ChangeEvent<HTMLInputElement>)=>{
+    setNotes(notes.map(item=>(
+     (item.id===Number(e.target.value))?{...item,completed:!item.completed}:item
+    )))
+   }
+   const deleteHandler=(id:number)=>{
+     const notesClone=[...notes];
+     const filteredNotes= notesClone.filter(item=>item.id!==id)
+      setNotes(filteredNotes)
+   }
   const sendDataHandler=(note:INote)=>{
     setNotes([...notes,note])
   }
-const filteredNotes=notes;
+
+  //! filter section------------------------------------
+let filteredNotes=notes;
 if(selectedOption==="earliest"){
   filteredNotes.sort((a,b) => +new Date(a.createdAt)- +new Date(b.createdAt))
 }else if(selectedOption==="completed"){
-  filteredNotes.sort((a,b) => +(a.completed)- +(b.completed))
+  filteredNotes=[...notes].sort((a,b) => +(a.completed)- +(b.completed))
 }else{
-  filteredNotes.sort((a,b) => +new Date(b.createdAt)- +new Date(a.createdAt))
+  filteredNotes=[...notes].sort((a,b) => +new Date(b.createdAt)- +new Date(a.createdAt))
 }
-  const changeCompleted=(e: React.ChangeEvent<HTMLInputElement>)=>{
-    console.log(Number(e.target.value))
-   setNotes(notes.map(item=>(
-    (item.id===Number(e.target.value))?{...item,completed:!item.completed}:item
-   )))
-  }
-  const deleteHandler=(id:number)=>{
-    const notesClone=[...notes];
-    const filteredNotes= notesClone.filter(item=>item.id!==id)
-     setNotes(filteredNotes)
-  }
 
   return (
     // <div className="App_container">
@@ -40,7 +46,8 @@ if(selectedOption==="earliest"){
     //   <Tab />
     // </div>
     <div className="NoteApp_container">
-      <Header selectedOption={selectedOption} changeOption={(e)=>setSelectedOption((e.target.value)as "latest"|"earliest"|"completed")} />
+      <Header selectedOption={selectedOption} 
+      changeOption={(e)=>setSelectedOption((e.target.value)as TOptions)} />
       <Main filteredNotes={filteredNotes} changeCompleted={changeCompleted} deleteHandler={deleteHandler} 
       notes={notes} sendDataHandler={sendDataHandler} />
     </div>
